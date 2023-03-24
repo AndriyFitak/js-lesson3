4f2e4eb7f758199c768a6a3514f197b563adb1d0
AndriyFitak <116104837+AndriyFitak@users.noreply.github.com>
2023-03-24 22:43:49 +0200

    js

--- script.js
+++ script.js
@@ -1,48 +1,65 @@
-/*let arr = []
-let n = 10
-
-
+let arr = []
+let n = Math.floor(Math.random()*(10))
 for (let i = 0; i < n; i++) {
-    arr[i] = Math.floor(Math.random()*(100-50))
-
+    arr[i]=Math.floor( Math.random()*(10))
 }
-
 console.log(arr)
-
-let minelement = arr[0]
-for (let i = 0; i < arr.length; i++) {
-    if (arr[i] < minelement){
-        minelement = arr[i]
-    }
-}
-console.log(minelement)*/
-/*for(let i = 0; i < arr.length; i++){
-    console.log(arr[i])
-}*/
-/*let sum = 0
+let sum = 0
 for (let i = 0; i < arr.length; i++) {
     sum += arr[i]
-    //
+    
 }
-console.log (sum)*/
+console.log(`Сума чисел ${sum}`)
+console.log(`Всього чисел ${arr.length}`)
 
-let colors = ["red" , "blue", "green", "yellow"]
-/*let searchColor = 'red'
+let minElement = arr[0]
+for(let i = 1; i < arr.length; i++) {
+    if (arr[i] < minElement) {
+        minElement = arr[i]
+    }
+}
+console.log(`Найменше число ${minElement}`)
+
+let maxElement = arr[0]
+for(let i = 1; i < arr.length; i++) {
+    if (arr[i] > maxElement)  {
+        maxElement = arr[i]
+    }
+}
+
+console.log(`Найбільше число ${maxElement}`)
+
+const numbers = arr
+const sums = numbers.reduce((acc, curr) => acc + curr);
+const average = sums / numbers.length;
+console.log (Math.floor(average));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let colors = ["Михайло", "Андрій", "Рома", "Мартін" , "Юра" ,"Галина","Софія"]
+let search = prompt("Введіть своє Імя")
 let searchStatus = false 
 
-for(let color of colors){
-    if(color == searchColor){
+for ( let color of  colors){
+    if( color == search){
         searchStatus = true
     }
 }
-if(searchStatus == true){
-    console.log('We  found color')  
-}else {
-    console.log("We not found color")
-}*/
-if(colors.indexOf("blue") != -1){
-  console.log("We found color")
-}else{
-    console.log("We not found color")
 
-}
+if(searchStatus == true){
+    console.log('Ми найшли ваше імя в базі')
+} else{
+    console.log('Ми не найшли ваше імя в базі')
+}*/
+
